0f2479d3c068f413d98b1126548445905dab4ae7
lbrady351 <104196776+lbrady351@users.noreply.github.com>
2022-05-09 12:01:09 -0500

    updated test file

--- test_gui_main.py
+++ test_gui_main.py
@@ -4,11 +4,11 @@ from gui_main import *
 
 class MyTestCase(unittest.TestCase):
     def test_student_name(self):
-        self.assertEqual(student_name("Abby"), "Abby")  # add assertion here
-        self.assertEqual(student_name("Beth"), "Beth")
+        self.assertEqual(student_name("Abby"), "ABBY")  # add assertion here
+        self.assertEqual(student_name("Beth"), "BETH")
 
-        self.assertNotEqual(student_name("Abby!"), "Abby")
-        self.assertNotEqual(student_name("123"), "Beth")
+        self.assertNotEqual(student_name("Abby!"), "ABBY!")
+        self.assertNotEqual(student_name("123"), "BETH")
 
     def test_student_score(self):
         self.assertEqual(student_score("50"), 50.0)
@@ -19,13 +19,13 @@ class MyTestCase(unittest.TestCase):
         self.assertNotEqual(student_score("hi"), 100)
 
     def test_letter_grade(self):
-        self.assertEqual(student_score("50"), "F")
-        self.assertEqual(student_score("100"), "A")
+        self.assertEqual(letter_grade("50"), "F")
+        self.assertEqual(letter_grade("100"), "A")
 
-        self.assertNotEqual(student_score("1000"), "A")
-        self.assertNotEqual(student_score("-5"), "F")
-        self.assertNotEqual(student_score("hi"), "F")
-        self.assertNotEqual(student_score("50"), "A")
+        self.assertNotEqual(letter_grade("1000"), "A")
+        self.assertNotEqual(letter_grade("-5"), "F")
+        self.assertNotEqual(letter_grade("hi"), "F")
+        self.assertNotEqual(letter_grade("50"), "A")
 
 
 if __name__ == '__main__':
